128169399eaf88ae267892908f1d01dfd25cbd92
andresguezz <jarodriguez3203@misena.edu.co>
2023-09-19 19:30:02 -0500

    conversion from HMTL to VUe

---
+++ client/src/components/Searchbar.vue
@@ -0,0 +1,39 @@
+<template>
+    <section class="search-bar" style="position: relative; background: #FEFCFB; margin-top: none; padding: 30px 0;">
+      <div class="container">
+        <div class="row justify-content-center">
+          <div class="col-md-8">
+            <div class="search-box" style="background: #FFFFFF; box-shadow: 0px 2px 80px 2px rgba(0, 0, 0, 0.06); border-radius: 10px; padding: 20px; display: flex; justify-content: space-between; align-items: center;">
+              <div class="d-flex">
+                <div class="text-left mb-3" style="margin-right: 50px;">
+                  <h3 style="font-family: 'Poppins'; font-weight: 500; font-size: 18px; color: #222222;">Location</h3>
+                  <p style="font-family: 'Poppins'; font-weight: 400; font-size: 12px; color: #666666;">Where are you going</p>
+                </div>
+                <div class="text-left mb-3" style="margin-right: 50px;">
+                  <h3 style="font-family: 'Poppins'; font-weight: 500; font-size: 18px; color: #222222;">Date</h3>
+                  <p style="font-family: 'Poppins'; font-weight: 400; font-size: 12px; color: #666666;">When will you go</p>
+                </div>
+                <div class="text-left mb-3" style="margin-right: 30px;">
+                  <h3 style="font-family: 'Poppins'; font-weight: 500; font-size: 18px; color: #222222;">Guests</h3>
+                  <p style="font-family: 'Poppins'; font-weight: 400; font-size: 12px; color: #666666;">Number of guests</p>
+                </div>
+              </div>
+              <div class="text-left">
+                <a href="#" class="btn btn-light" style="background-color: #FA7436; font-family: 'Poppins'; font-weight: 500; font-size: 18px; padding: 10px 30px; border: none;">Explore Now</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </template>
+  
+  <script>
+  export default {
+    name: 'Searchbar'
+  };
+  </script>
+  
+  <style scoped>
+  /* Agrega estilos CSS si es necesario */
+  </style>
