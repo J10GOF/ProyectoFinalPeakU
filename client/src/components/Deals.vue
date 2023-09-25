860cc0b80b08b35bdd678d23eb4ef865088a52d5
Juan Diego Flórez <93104172+J10GOF@users.noreply.github.com>
2023-09-25 07:53:54 -0500

    modificaciones esteticas en el codigo

--- client/src/components/Deals.vue
+++ client/src/components/Deals.vue
@@ -1,64 +1,139 @@
 <template>
-    <section class="exclusive-deals mb-5" style="position: relative; background: #FEFCFB;">
-      <div class="container">
-        <div class="row">
-          <div class="col-lg-12 text-center">
-            <h2 class="font-weight-bold mt-5 mb-3" style="font-family: 'Volkhov', sans-serif; font-size: 48px; color: #222222;">Exclusive <span style="color: #FA7436;">deals & discounts</span></h2>
-            <div class="col-md-12">
-              <p class="font-weight-light mb-2" style="font-family: 'Poppins', sans-serif; font-size: 18px; color: #666666;">Discover our fantastic early booking discounts</p>
-              <p class="font-weight-light mb-5" style="font-family: 'Poppins', sans-serif; font-size: 18px; color: #666666;">and start planning your journey.</p>
-            </div>
+  <section
+    class="exclusive-deals mb-5"
+    style="position: relative; background: #fefcfb"
+  >
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12 text-center">
+          <h2
+            class="font-weight-bold mt-5 mb-3"
+            style="
+              font-family: 'Volkhov', sans-serif;
+              font-size: 48px;
+              color: #222222;
+            "
+          >
+            Exclusive <span style="color: #fa7436">deals & discounts</span>
+          </h2>
+          <div class="col-md-12">
+            <p
+              class="font-weight-light mb-2"
+              style="
+                font-family: 'Poppins', sans-serif;
+                font-size: 18px;
+                color: #666666;
+              "
+            >
+              Discover our fantastic early booking discounts
+            </p>
+            <p
+              class="font-weight-light mb-5"
+              style="
+                font-family: 'Poppins', sans-serif;
+                font-size: 18px;
+                color: #666666;
+              "
+            >
+              and start planning your journey.
+            </p>
           </div>
         </div>
-        <div class="row">
-          <div class="col-lg-12">
-            <div class="card-container">
-              <div class="col-lg-3 mb-4" v-for="(deal) in visibleDeals" :key="deal.id">
-                <div class="card bg-light border-0">
-                <img :src="deal.image" class="card-img-top" alt="...">
+      </div>
+      <div class="row">
+        <div class="col-lg-12">
+          <div class="card-container">
+            <div
+              class="col-lg-3 mb-4"
+              v-for="deal in visibleDeals"
+              :key="deal.id"
+            >
+              <div class="card bg-light border-0">
+                <img :src="deal.image" class="card-img-top" alt="..." />
                 <div class="card-body">
                   <h5 class="card-title">{{ deal.title }}</h5>
                   <div class="d-flex align-items-center justify-content-center">
-                    <img src="../assets/deals/star.png" alt="Star" class="mr-1">
-                    <span class="font-weight-bold text-muted">{{ deal.rating }}</span>
+                    <img
+                      src="../assets/deals/star.png"
+                      alt="Star"
+                      class="mr-1"
+                    />
+                    <span class="font-weight-bold text-muted">{{
+                      deal.rating
+                    }}</span>
                   </div>
                   <div class="d-flex align-items-center justify-content-left">
-                    <img src="../assets/deals/Vector(1).png" alt="vector-point" class="mr-3" style="margin-right: 5px; width: 10px;">
+                    <img
+                      src="../assets/deals/Vector(1).png"
+                      alt="vector-point"
+                      class="mr-3"
+                      style="margin-right: 5px; width: 10px"
+                    />
                     <p class="card-text text-muted mt-2">{{ deal.location }}</p>
                   </div>
-                  <div class="d-flex justify-content-between align-items-center">
-                    <del class="font-weight-light">{{ deal.originalPrice }}</del>
-                    <span class="font-weight-bold" style="color: #FA7436; background-color: #FFE7DB;">{{ deal.discountedPrice }}</span>
+                  <div
+                    class="d-flex justify-content-between align-items-center"
+                  >
+                    <del class="font-weight-light">{{
+                      deal.originalPrice
+                    }}</del>
+                    <span
+                      class="font-weight-bold"
+                      style="color: #fa7436; background-color: #ffe7db"
+                      >{{ deal.discountedPrice }}</span
+                    >
                   </div>
-                </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-        <div class="row mt-3 mb-5">
-        <div class="col-12 text-center" style="margin-bottom: 200px;">
-          <button class="btn btn-dark rounded-circle" style="width: 40px; height: 40px; margin-right: 30px; background-color: #FEFCFB;" @click="prevSlide">
-            <span><img src="../assets/deals/arrow-left.svg" style="width: 20px;"></span>
+      </div>
+      <div class="row mt-3 mb-5">
+        <div class="col-12 text-center" style="margin-bottom: 200px">
+          <button
+            class="btn btn-dark rounded-circle"
+            style="
+              width: 40px;
+              height: 40px;
+              margin-right: 30px;
+              background-color: #fefcfb;
+            "
+            @click="prevSlide"
+          >
+            <span
+              ><img src="../assets/deals/arrow-left.svg" style="width: 20px"
+            /></span>
           </button>
-          <button class="btn btn-dark rounded-circle" style="width: 40px; height: 40px; background-color: #FEFCFB;" @click="nextSlide">
-            <span><img src="../assets/deals/arrow-right.svg" style="width: 20px; color: #FFFFFF;"></span>
+          <button
+            class="btn btn-dark rounded-circle"
+            style="width: 40px; height: 40px; background-color: #fefcfb"
+            @click="nextSlide"
+          >
+            <span
+              ><img
+                src="../assets/deals/arrow-right.svg"
+                style="width: 20px; color: #ffffff"
+            /></span>
           </button>
         </div>
       </div>
     </div>
   </section>
 </template>
-  
+
 <script>
+import { Snackbar, Alert } from "@mui/material";
+
 export default {
-  name: 'Deals',
+  name: "Deals",
   data() {
     return {
       // Tu array de ofertas vacío inicialmente
       deals: [],
       currentPosition: 0,
       cardsToShow: 3,
+      showAlert: false, // Bandera para mostrar la alerta
     };
   },
   computed: {
@@ -83,14 +158,14 @@ export default {
     },
     fetchData() {
       // Realizar la solicitud HTTP GET a tu backend
-      fetch('http://localhost:3001/place')
-        .then(response => response.json())
-        .then(data => {
+      fetch("http://localhost:3001/place")
+        .then((response) => response.json())
+        .then((data) => {
           // Asignar los datos recibidos a la variable 'deals'
           this.deals = data;
         })
-        .catch(error => {
-          console.error('Error al cargar los datos:', error);
+        .catch((error) => {
+          console.error("Error al cargar los datos:", error);
         });
     },
   },
@@ -98,24 +173,34 @@ export default {
     // Llamar a la función fetchData cuando se monta el componente
     this.fetchData();
   },
+  computed: {
+    visibleDeals() {
+      // Calcular las ofertas visibles en función de la posición actual y el número de cartas a mostrar
+      const startIndex = this.currentPosition;
+      const endIndex = startIndex + this.cardsToShow;
+      const visibleDeals = this.deals.slice(startIndex, endIndex);
+
+      // Muestra la alerta si no hay ofertas visibles
+      this.showAlert = visibleDeals.length === 0;
+
+      return visibleDeals;
+    },
+  },
 };
 </script>
-  
-  <style scoped>
-  .card-container {
+
+<style scoped>
+.card-container {
   display: flex;
   justify-content: space-between;
   align-items: flex-start;
   flex-wrap: nowrap;
-  }
+}
 .card-container .col-lg-3 {
-  margin-right: 10px; 
+  margin-right: 10px;
 }
 
 .card-container .col-lg-3:last-child {
-  margin-right: 0; 
+  margin-right: 0;
 }
-
-
-  </style>
-  
+</style>
--- client/src/components/Searchbar.vue
+++ client/src/components/Searchbar.vue
@@ -1,39 +1,132 @@
 <template>
-    <section class="search-bar" style="position: relative; background: #FEFCFB; margin-top: none; padding: 30px 0;">
-      <div class="container">
-        <div class="row justify-content-center">
-          <div class="col-md-8">
-            <div class="search-box" style="background: #FFFFFF; box-shadow: 0px 2px 80px 2px rgba(0, 0, 0, 0.06); border-radius: 10px; padding: 20px; display: flex; justify-content: space-between; align-items: center;">
-              <div class="d-flex">
-                <div class="text-left mb-3" style="margin-right: 50px;">
-                  <h3 style="font-family: 'Poppins'; font-weight: 500; font-size: 18px; color: #222222;">Location</h3>
-                  <p style="font-family: 'Poppins'; font-weight: 400; font-size: 12px; color: #666666;">Where are you going</p>
-                </div>
-                <div class="text-left mb-3" style="margin-right: 50px;">
-                  <h3 style="font-family: 'Poppins'; font-weight: 500; font-size: 18px; color: #222222;">Date</h3>
-                  <p style="font-family: 'Poppins'; font-weight: 400; font-size: 12px; color: #666666;">When will you go</p>
-                </div>
-                <div class="text-left mb-3" style="margin-right: 30px;">
-                  <h3 style="font-family: 'Poppins'; font-weight: 500; font-size: 18px; color: #222222;">Guests</h3>
-                  <p style="font-family: 'Poppins'; font-weight: 400; font-size: 12px; color: #666666;">Number of guests</p>
-                </div>
+  <section class="search-bar" style="position: relative; background: #FEFCFB; margin-top: none; padding: 30px 0;">
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-md-8">
+          <div class="search-box" style="background: #FFFFFF; box-shadow: 0px 2px 80px 2px rgba(0, 0, 0, 0.06); border-radius: 10px; padding: 20px; display: flex; justify-content: space-between; align-items: center;">
+            <div class="d-flex">
+              <div class="text-left mb-3" style="margin-right: 50px;">
+                <h3 style="font-family: 'Poppins'; font-weight: 500; font-size: 18px; color: #222222;">Location</h3>
+                <p style="font-family: 'Poppins'; font-weight: 400; font-size: 12px; color: #666666;">Where are you going</p>
+                <input type="text" v-model="selectedCountry" @input="filterResults" placeholder="Select a country">
               </div>
-              <div class="text-left">
-                <a href="#" class="btn btn-light" style="background-color: #FA7436; font-family: 'Poppins'; font-weight: 500; font-size: 18px; padding: 10px 30px; border: none;">Explore Now</a>
+              <div class="text-left mb-3" style="margin-right: 50px;">
+                <h3 style="font-family: 'Poppins'; font-weight: 500; font-size: 18px; color: #222222;">Discount</h3>
+                <p style="font-family: 'Poppins'; font-weight: 400; font-size: 12px; color: #666666;">Filter by discount</p>
+                <input type="number" v-model="selectedDiscount" @input="filterResults" placeholder="Enter discount">
               </div>
+              <div class="text-left mb-3" style="margin-right: 30px;">
+                <h3 style="font-family: 'Poppins'; font-weight: 500; font-size: 18px; color: #222222;">Price</h3>
+                <p style="font-family: 'Poppins'; font-weight: 400; font-size: 12px; color: #666666;">Filter by price</p>
+                <input type="number" v-model="selectedPrice" @input="filterResults" placeholder="Enter price">
+              </div>
+            </div>
+            <div class="text-left">
+              <a href="#" class="btn btn-light" @click="openModal" style="background-color: #FA7436; font-family: 'Poppins'; font-weight: 500; font-size: 18px; padding: 10px 30px; border: none;">Explore Now</a>
             </div>
           </div>
         </div>
       </div>
-    </section>
-  </template>
-  
-  <script>
-  export default {
-    name: 'Searchbar'
-  };
-  </script>
-  
-  <style scoped>
-  /* Agrega estilos CSS si es necesario */
-  </style>
+    </div>
+    <div v-if="showModal" class="modal">
+      <div class="modal-content">
+        <div class="modal-header">
+          <span class="close" @click="closeModal">&times;</span>
+          <h2>Filtered Results</h2>
+        </div>
+        <div class="modal-body">
+          <div v-for="result in filteredResults" :key="result.id" class="card">
+            <!-- Display filtered results here -->
+            <!-- You can customize how the results are displayed -->
+            <h3>{{ result.name }}</h3>
+            <p>Country: {{ result.country }}</p>
+            <p>Discount: {{ result.discount }}</p>
+            <p>Price: {{ result.price }}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'Searchbar',
+  data() {
+    return {
+      selectedCountry: '',
+      selectedDiscount: null,
+      selectedPrice: null,
+      showModal: false,
+      results: [], // Store API results here
+      filteredResults: [] // Store filtered results here
+    };
+  },
+  methods: {
+    filterResults() {
+      // Implement your filtering logic here
+      // You can use this.selectedCountry, this.selectedDiscount, and this.selectedPrice to filter results
+      // Update this.filteredResults with the filtered results
+    },
+    openModal() {
+      // Make a GET request to the API http://localhost:3001/place
+      // Update this.results with the API response
+      // Set this.showModal to true to open the modal
+    },
+    closeModal() {
+      this.showModal = false;
+    }
+  }
+};
+</script>
+
+<style scoped>
+/* Agrega estilos CSS si es necesario */
+.modal {
+  display: none;
+  position: fixed;
+  z-index: 1;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  overflow: auto;
+  background-color: rgba(0,0,0,0.4);
+}
+
+.modal-content {
+  background-color: #fefefe;
+  margin: 15% auto;
+  padding: 20px;
+  border: 1px solid #888;
+  width: 80%;
+}
+
+.modal-header {
+  padding: 2px 16px;
+  background-color: #5bc0de;
+  color: white;
+}
+
+.close {
+  color: #aaa;
+  float: right;
+  font-size: 28px;
+  font-weight: bold;
+}
+
+.close:hover,
+.close:focus {
+  color: black;
+  text-decoration: none;
+  cursor: pointer;
+}
+
+.card {
+  margin: 10px;
+  padding: 10px;
+  border: 1px solid #ddd;
+  border-radius: 5px;
+  background-color: #f9f9f9;
+}
+</style>
